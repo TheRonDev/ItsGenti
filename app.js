db1bbc3852ac57710f2a2fb22f8e3d9c3fed102d
TheRonDev <58440300+TheRonDev@users.noreply.github.com>
2021-01-27 13:22:52 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+function revealMessage () {
+    document.getElementById("hiddenMessage").style.display = "block"
+}
+
+setTimeout(function(){
+    document.getElementById('hiddenMessage').style.display = 'none';
+}, 10000); 
+
